9fdecac56f87f30eda01980da50fa5e54389bd91
yyyu200 <yyyu200@163.com>
2019-10-19 20:24:06 +0800

    modify at20191019-20:24:06-aliyun

--- transcell.py
+++ transcell.py
@@ -177,3 +177,8 @@ if __name__ == '__main__':
     sc=CELL("sc.vasp")
     fcc=CELL.unit2prim(sc,2)
     fcc.print_poscar("fcc.vasp")
+    
+    in2o3_unit=CELL("In2O3.vasp")
+    bcc=CELL.unit2prim(in2o3_unit,3)
+    bcc.print_poscar("bcc.vasp")
+
