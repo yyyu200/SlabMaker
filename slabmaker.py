73a457738912a0f8d4b8ab0737c59a2c739b7c5f
yyyu200 <yyyu200@163.com>
2019-10-19 21:06:36 +0800

    modify at20191019-21:06:36-aliyun

---
+++ slabmaker.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" @author: yyyu200@163.com """
+
+import numpy as np
+from CELL import CELL
+
+
--- transcell.py
+++ transcell.py
@@ -4,181 +4,21 @@
 """ @author: yyyu200@163.com """
 
 import numpy as np
-import copy
+from CELL import CELL
 
-class CELL(object):
-    close_thr=1.0e-4
-    def __init__(self, fnam, fmt='POSCAR'):
-        '''
-        init from POSCAR
-        '''
-        if fmt=='POSCAR':
-            fi=open(fnam)
-            ll=fi.readlines() 
-            self.system=ll[0]
-            self.alat=float(ll[1])
-            self.cell=np.zeros([3,3])
-            for i in range(3):
-                for j in range(3):
-                    self.cell[i,j]=float(ll[2+i].split()[j])
-            self.ntyp=len(ll[5].split())
-            self.typ_name=ll[5].split()
-            self.typ_num=np.zeros([self.ntyp],dtype=np.int32)
-            for i in range(self.ntyp):
-                self.typ_num[i]=int(ll[6].split()[i])
-            self.coordsystem=ll[7] # Direct only, no 'Selective Dynamics' line expected
-            self.nat=int(sum(self.typ_num))
-            self.attyp=np.zeros([self.nat],dtype=np.int32)
-            k=0
-            for i in range(self.ntyp):
-                for j in range(self.typ_num[i]):
-                    self.attyp[k]=i
-                    k+=1
+c1=CELL("mC.vasp")
+prim=CELL.unit2prim(c1,13)
+prim.print_poscar("prim.vasp")
 
-            self.atpos=np.zeros([self.nat,3],dtype=np.float64)
-            for i in range(self.nat):
-                for j in range(3):
-                    self.atpos[i,j]=float(ll[8+i].split()[j])
-            
-            fi.close() 
-        elif fmt=='QE':
-            pass
-        else:
-            raise NotImplementedError
+hexcell=CELL("Al2O3.vasp")
+rhcell=CELL.unit2prim(hexcell,5)
+rhcell.print_poscar("rh.vasp")
 
-    def __str__(self):
-        return "cell\n"+self.cell.__str__() + "\natom positions:\n"+ self.atpos.__str__()
+sc=CELL("sc.vasp")
+fcc=CELL.unit2prim(sc,2)
+fcc.print_poscar("fcc.vasp")
 
-    @staticmethod
-    def dist(a,b):
-        return np.linalg.norm(a-b)
-
-    def unique(self):
-        n=self.nat
-        i_kind=np.zeros([n],dtype=np.int32)
-        for i in range(n):
-            i_kind[i]=i
-
-        for i in range(n):
-            for j in range(i,n):
-                if CELL.dist(self.atpos[i],self.atpos[j])<self.close_thr:
-                    assert(self.attyp[i]==self.attyp[j])
-                    i_kind[i]=i_kind[i]
-                    i_kind[j]=i_kind[i]
-        uniq=[]
-        typ_of_uniq=[]
-        for i in np.unique(i_kind):
-            uniq.append(self.atpos[i])
-            typ_of_uniq.append(self.attyp[i])
-        
-        #print(i_kind)
-        self.nat=len(uniq)
-        self.atpos=np.array(uniq)
-        self.attyp=np.array(typ_of_uniq)
-
-        
-        for i in range(self.ntyp):
-            self.typ_num[i]=0
-            for j in range(self.nat):
-                if self.attyp[j]==i:
-                    self.typ_num[i]+=1
-
-    def tidy_up(self): # tanslate to [0-1), sort by z axis
-        for i in range(self.nat):
-            for j in range(3):
-                tmp_f, tmp_i=np.modf(self.atpos[i][j]) # the fractional part , the interger part
-                if tmp_f < 0.0:
-                    self.atpos[i][j]=tmp_f+1.0
-                else:
-                    self.atpos[i][j]=tmp_f
-
-        self.at_sort()
-
-    def at_sort(self): # sort by element type
-        tmp=self.attyp[:].argsort(kind='mergesort')
-        self.attyp=self.attyp[tmp]
-        self.atpos=self.atpos[tmp]
-        #tmp=self.atpos[:,2].argsort(kind='mergesort')
-
-
-    def print_poscar(self,fnam):
-        '''
-        print to POSCAR
-        '''
-        fo=open(fnam,"w")
-        fo.write("system: "+' '.join(self.typ_name)+"\n")
-        fo.write("1.0\n")
-        for i in range(3):
-            for j in range(3):
-                fo.write(" %f" % self.cell[i,j])
-            fo.write("\n")
-        for i in range(self.ntyp):
-            fo.write(self.typ_name[i]+" ")
-        fo.write("\n")
-        for i in range(self.ntyp):
-            fo.write(str(self.typ_num[i])+" ")
-        fo.write("\n")
-        fo.write("Direct\n")
-        for i in range(self.nat):
-            for j in range(3):
-                #dig=np.modf(self.atpos[i][j])[0]
-                #if dig < 0:
-                #    dig=dig+1.0
-                fo.write(" %.12f" % ( self.atpos[i][j]))
-            fo.write("\n")
-    
-    def findfour():
-        pass    
-
-    def findprim():
-        findfour()
-        pass
-
-    @staticmethod
-    def unit2prim(unitcell, ibrav):
-        primcell=copy.deepcopy(unitcell)
-
-        if ibrav in [2,10]: # face center
-            P=np.mat([[0.0, 0.5, 0.5],[0.5, 0.0, 0.5],[0.5, 0.5, 0.0]],dtype=np.float64)
-            Q=np.mat([[-1,1,1],[1,-1,1],[1,1,-1]],dtype=np.float64)
-        elif ibrav in [3,7,11]: # body center
-            P=np.mat([[-0.5, 0.5, 0.5],[0.5, -0.5, 0.5],[0.5, 0.5, -0.5]],dtype=np.float64)
-            Q=np.mat([[0,1,1],[1,0,1],[1,1,0]],dtype=np.float64)
-        elif ibrav==5: # rhombohedral
-            P=np.mat([[2/3.0,-1/3.0,-1/3.0],[1/3.0,1/3.0,-2/3.0],[1/3.0,1/3.0,1/3.0]],dtype=np.float64)
-            Q=np.mat([[1,0,1],[-1,1,1],[0,-1,1]],dtype=np.float64)
-        elif ibrav in [9,13]: # base center
-            P=np.mat([[0.5,-0.5,0],[0.5,0.5,0],[0,0,1.0]],dtype=np.float64)
-            Q=np.mat([[1,1,0],[-1,1,0],[0,0,1]],dtype=np.float64)
-        else:
-            print("unit cell is primitive for ibrav: ", ibrav)
-            P=np.mat([[1,0,0],[0,1,0],[0,0,1]], dtype=np.float64)
-            Q=np.mat([[1,0,0],[0,1,0],[0,0,1]], dtype=np.float64)
-
-        primcell.cell=(np.mat(unitcell.cell).T*P).T
-        primcell.nat=unitcell.nat
-        for i in range(primcell.nat):
-            primcell.atpos[i]=np.array(Q*(np.mat(unitcell.atpos[i]).T)).flatten()
-        primcell.tidy_up()
-        primcell.unique()
-
-        return primcell
-
-
-if __name__ == '__main__':
-    c1=CELL("mC.vasp")
-    prim=CELL.unit2prim(c1,13)
-    prim.print_poscar("prim.vasp")
-
-    hexcell=CELL("Al2O3.vasp")
-    rhcell=CELL.unit2prim(hexcell,5)
-    rhcell.print_poscar("rh.vasp")
-
-    sc=CELL("sc.vasp")
-    fcc=CELL.unit2prim(sc,2)
-    fcc.print_poscar("fcc.vasp")
-    
-    in2o3_unit=CELL("In2O3.vasp")
-    bcc=CELL.unit2prim(in2o3_unit,3)
-    bcc.print_poscar("bcc.vasp")
+in2o3_unit=CELL("In2O3.vasp")
+bcc=CELL.unit2prim(in2o3_unit,3)
+bcc.print_poscar("bcc.vasp")
 
